748822368506a7d5b8dcaac5dfd953009e345665
MagomedovNR <n.magomedov@id-mt.ru>
2025-11-24 22:43:02 +0300

    l1-11-update, readme

--- L1-11/main.go
+++ L1-11/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Пересечение множеств
 
 // Реализовать пересечение двух неупорядоченных множеств
@@ -12,5 +14,27 @@ package main
 // Пересечение = {2,3}
 
 func main() {
+	a := []int{1, 2, 3, 7, 8, 9}
+	b := []int{2, 3, 4, 9, 8, 7}
+	x := intersection(a, b)
+	fmt.Println(x)
+}
 
+func intersection(a []int, b []int) []int {
+	setA := make(map[int]bool)
+	for _, v := range a {
+		setA[v] = true
+	}
+
+	result := []int{}
+	seen := make(map[int]bool) // для избежания дубликатов
+
+	for _, v := range b {
+		if setA[v] && !seen[v] {
+			result = append(result, v)
+			seen[v] = true
+		}
+	}
+
+	return result
 }

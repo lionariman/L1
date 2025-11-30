f8f15051b1e7dd7e10b2133beea8b13bf8cb11af
MagomedovNR <n.magomedov@id-mt.ru>
2025-11-30 17:52:08 +0300

    l1-13, readme

---
+++ L1-13/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// Обмен значениями без третьей переменной
+
+// Поменять местами два числа без использования временной переменной.
+// Подсказка: примените сложение/вычитание или XOR-обмен.
+
+func main() {
+	vr1 := 123
+	vr2 := 456
+
+	fmt.Println("SWAP BY ADD/SUBSTRUCT")
+	fmt.Println("BEFORE:", vr1, vr2)
+	swapByAddSubstruct(&vr1, &vr2)
+	fmt.Println(" AFTER:", vr1, vr2)
+	
+	fmt.Println("\nSWAP BY XOR")
+	fmt.Println("BEFORE:", vr1, vr2)
+	swapByXOR(&vr1, &vr2)
+	fmt.Println(" AFTER:", vr1, vr2)
+	
+}
+
+func swapByAddSubstruct(num1 *int, num2 *int) {
+	*num1 = *num1 + *num2
+	*num2 = *num1 - *num2
+	*num1 = *num1 - *num2
+}
+
+func swapByXOR(num1 *int, num2 *int) {
+	*num1 = *num1 ^ *num2
+	*num2 = *num1 ^ *num2
+	*num1 = *num1 ^ *num2
+}

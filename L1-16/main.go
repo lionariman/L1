32728e542357611d25542a366b6f88a28ed4d296
MagomedovNR <n.magomedov@id-mt.ru>
2025-12-02 02:13:46 +0300

    l1-16, finish

--- L1-16/main.go
+++ L1-16/main.go
@@ -12,5 +12,32 @@ import "fmt"
 // Для выбора опорного элемента можно взять середину или первый элемент.
 	
 func main() {
-	fmt.Println(1)
+	unsortedNumbers := []int{6, 3, 8, 1, 4, 7, 2}
+	sortedNumbers := quickSort(unsortedNumbers)
+	
+	fmt.Println(unsortedNumbers, "\n", sortedNumbers)
+}
+
+func quickSort(numbers []int) []int {
+	if len(numbers) <= 1 {
+		return numbers
+	}
+	pivot := len(numbers)/2
+	left := []int{}
+	right := []int{numbers[pivot]}
+	for i, val := range numbers {
+		if i == pivot {
+			continue
+		}
+		if val < numbers[pivot] {
+			left = append(left, val)
+		} else {
+			right = append(right, val)
+		}
+	}
+	
+	left = quickSort(left)
+	right = quickSort(right)
+	
+	return append(left, right...)
 }

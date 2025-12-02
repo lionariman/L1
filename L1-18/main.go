3c1733b24a0fe62c1ed08cc811453ad5031b328b
MagomedovNR <n.magomedov@id-mt.ru>
2025-12-03 01:53:33 +0300

    l1-18, first version

---
+++ L1-18/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Конкурентный счетчик
+
+// Реализовать структуру-счётчик, которая будет инкрементироваться
+// в конкурентной среде (т.е. из нескольких горутин).
+// По завершению программы структура должна выводить итоговое значение счётчика.
+
+// Подсказка: вам понадобится механизм синхронизации,
+// например, sync.Mutex или sync/Atomic для безопасного инкремента.
+
+type Counter struct {
+	Count int
+}
+
+func (cn *Counter) Increment() {
+	cn.Count += 1
+}
+
+func main() {
+	counter := Counter{0}
+	
+	mt := sync.Mutex{}
+	wg := sync.WaitGroup{}
+	
+	gnum := 3
+	
+	wg.Add(gnum)
+	
+	for i := range gnum {
+		go func(j int, cnt *Counter) {
+			defer wg.Done()
+			mt.Lock()
+			cnt.Increment()
+			fmt.Println("Goroutine", j, " made increment >", cnt.Count)
+			time.Sleep(1 * time.Second)
+			mt.Unlock()
+		}(i, &counter)
+	}
+	
+	wg.Wait()
+	
+	fmt.Println("Result of incrementation:", counter.Count)
+}

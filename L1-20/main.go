db0e8edafc51d325a9793e0004a2b5df7d0438a5
MagomedovNR <n.magomedov@id-mt.ru>
2025-12-03 18:36:10 +0300

    L1-20, last version

--- L1-20/main.go
+++ L1-20/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	// "strings"
 )
 
 // Разворот слов в предложении
@@ -21,24 +20,47 @@ func main() {
 }
 
 func reverseWords(words string) string {
-	arr := []rune{}
-	left := len(words)-1
-	right := len(words)-1
-	for left > -1 {
-		if words[left] == ' ' || left == 0 {
-			left2 := left
-			for left2 <= right {
-				if rune(words[left2]) != 32 {
-					arr = append(arr, rune(words[left2]))
-				}
-				left2++
-			}
-			if left > 0 {
-				arr = append(arr, 32)
-			}
-			right = left
+	arr := []rune(words)
+	reverse(arr, 0, len(words)-1)
+	start := 0
+	for i := 0; i <= len(arr); i++ {
+		if i == len(arr) || arr[i] == ' ' {
+			reverse(arr, start, i-1)
+			start = i + 1
 		}
-		left--
 	}
 	return string(arr)
 }
+
+func reverse(runes []rune, left, right int) {
+	for left < right {
+		runes[left], runes[right] = runes[right], runes[left]
+		left++
+		right--
+	}
+}
+
+// First version
+
+// func reverseWordsFirst(words string) string {
+// 	arr := []rune{}
+// 	left := len(words)-1
+// 	right := len(words)-1
+// 	for left > -1 {
+// 		if words[left] == ' ' || left == 0 {
+// 			left2 := left
+// 			for left2 <= right {
+// 				if rune(words[left2]) != 32 {
+// 					arr = append(arr, rune(words[left2]))
+// 				}
+// 				left2++
+// 			}
+// 			if left > 0 {
+// 				arr = append(arr, 32)
+// 			}
+// 			right = left
+// 		}
+// 		left--
+// 	}
+// 	return string(arr)
+// }

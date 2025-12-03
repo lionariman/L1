26d42f44f6b846ac003b7e0c140ac1b134708c29
MagomedovNR <n.magomedov@id-mt.ru>
2025-12-03 17:02:13 +0300

    L1-20, first version

--- L1-20/main.go
+++ L1-20/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	// "strings"
 )
 
 // Разворот слов в предложении
@@ -15,5 +16,29 @@ import (
 // Постарайтесь не использовать дополнительные срезы, а выполнять операцию «на месте».
 
 func main() {
-	fmt.Println(1)
+	res := reverseWords("sun dog snow")
+	fmt.Printf("%v", res)
+}
+
+func reverseWords(words string) string {
+	arr := []rune{}
+	left := len(words)-1
+	right := len(words)-1
+	for left > -1 {
+		if words[left] == ' ' || left == 0 {
+			left2 := left
+			for left2 <= right {
+				if rune(words[left2]) != 32 {
+					arr = append(arr, rune(words[left2]))
+				}
+				left2++
+			}
+			if left > 0 {
+				arr = append(arr, 32)
+			}
+			right = left
+		}
+		left--
+	}
+	return string(arr)
 }

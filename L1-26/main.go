21923b38d927649b197904caf8cd611208363b7b
MagomedovNR <n.magomedov@id-mt.ru>
2025-12-11 05:06:52 +0300

    L1-26 last version

--- L1-26/main.go
+++ L1-26/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 	
 // Уникальные символы в строке
 
@@ -22,5 +25,41 @@ import "fmt"
 // воспользоваться для проверки условия.
 
 func main() {
-	fmt.Println(0)
+	fmt.Println(unique("aA"))
+	// fmt.Println(byte('a')+32 - byte('A'))
 }
+
+// неэффективное решение, сложноность O(n)
+func unique(str string) bool {
+	seen := make(map[rune]bool)
+	for _, char := range strings.ToLower(str) {
+		if seen[char] {
+			return false
+		}
+		seen[char] = true
+	}
+	return true
+}
+
+// неэффективное решение, сложноность O(n^2)
+// func uniqueOld(str string) bool {
+// 	counter := 0
+// 	step := 0
+// 	for counter < len(str) && step < len(str) {
+// 		// fmt.Println(step, counter)
+// 		if counter != step {
+// 			if str[step] == str[counter] ||
+// 				(str[step]+32 - str[counter]) == 0 ||
+// 				(str[step]+32 - str[counter]) == 64 {
+// 				return false
+// 			}
+// 		}
+// 		if counter+1 == len(str) && step < len(str) {
+// 			counter = 0
+// 			step++
+// 			continue
+// 		}
+// 		counter++
+// 	}
+// 	return true
+// }
